d48f88d7f540a7ba4b83aca8af7f30157e183259
cruizherrejon <cruizherrejon@gmail.com>
2025-11-04 14:33:50 -0600

    first advance with rest (#15)

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+ 
 var app = builder.Build();
+ 
+app.UseCors("AllowAll");
 
-app.MapGet("/Hello/world", () => "Hello World!");
+app.MapGet("/Hello/world", () => "Hola mundo!");
 
 app.Run();
+
